65e5099950d78c9c65d6f79fdf8686e9496b9fe8
oguzhan eryigit <oguzhanerygt@gmail.com>
2022-11-20 19:54:37 +0300

    Db connection

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // imports
 const express = require('express');
-const bodyParser = require('body-parser');
-const { urlencoded } = require('express');
-require('./db/dbConnections');
-const ErrorMiddleware =require("./Middleware/ErrorMiddleware.js");
-const userRouter= require("./Routes/userRouter.js")
+// const bodyParser = require('body-parser');
+// const { urlencoded } = require('express');
+// require('./db/dbConnections');
+// const ErrorMiddleware =require("./Middleware/ErrorMiddleware.js");
+// const userRouter= require("./Routes/userRouter.js")
 
 // constants
 const PORT = 3000;
@@ -12,12 +12,32 @@ const PORT = 3000;
 // middlewares
 const app=express();
 app.use(express.json());
-app.use(urlencoded({extended:true}));
+// app.use(urlencoded({extended:true}));
 //app.use(bodyParser.json())
+let dbOptions={
+    port:5432,
+    host: 'localhost',
+    dialect: 'postgres'
+}
+let dbName="SecurityProjectDB"
+let userName="postgres"
+let password="1029"
 
-app.use("/api/users",userRouter);
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(dbName, userName, password, dbOptions);
 
-app.use(ErrorMiddleware);
+const f=async()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+f()
+// app.use("/api/users",userRouter);
+//
+// app.use(ErrorMiddleware);
 
 
 
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,1 +1,23 @@
-// postgre sql user model
+let dbOptions={
+    port:5432,
+    host: 'localhost',
+    dialect: 'postgres'
+}
+let dbName="SecurityProjectDB"
+let userName="postgres"
+let password="1029"
+
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(dbName, userName, password, dbOptions);
+
+const User = sequelize.define("user", {
+    userName: DataTypes.STRING,
+    password:DataTypes.STRING,
+    email:DataTypes.STRING,
+
+});
+
+(async () => {
+    await sequelize.sync({ force: true });
+    // Code here
+})();
